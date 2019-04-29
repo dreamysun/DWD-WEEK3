f380901d954a484392460654d36da5deb1c1fe4a
dreamy <sundreamysun@gmail.com>
2019-04-29 03:26:35 -0400

    make it better

--- index.js
+++ index.js
@@ -1,56 +1,70 @@
 const express = require('express')
-const mustacheExpress = require('mustache-express');
-const { Client } = require('pg')
-const bodyParser = require('body-parser')
-const url = require('url')
 const path = require('path')
+const bodyParser = require('body-parser')
+const mustacheExpress = require('mustache-express');
 
-let client
-if(process.env.DATABASE_URL){
-    client = new Client({connectionString: process.env.DATABASE_URL, ssl: true})
-}
-else{
-    client = new Client({database:'posts'})
-}
+const app = express ();
+const PORT = process.env.PORT || 5000
 
-client.connect()
-
-let app = express()
+var lost = [];
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname);
+app.use(bodyParser.json())          //json support
+app.use(bodyParser.urlencoded({    //url support
+    extended: true
+  }));
+app.use('/public', express.static('public'));
+const {Client} = require('pg');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+//HEROKU_POSTGRESQL_MAUVE_URL
 
-app.get('/', function(req, res){
-    client.query('SELECT * FROM posts', (err, resSQL) => {
-        if (err) {
-          console.log(err.stack)
-        } else {
-            let someArray = resSQL.rows
-            res.render('index', {
-                someArray
-            });
-        }
-      })
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl:true
+});
+
+client.connect();
+
+
+ app.get('/', async (req, res) => {
+    try {
+      //const client = await pool.connect()
+      const result = await client.query('SELECT * FROM forum');
+      //const results = { 'results': (result) ? result.rows : null};
+      //for (var i = 0; i < result.rows.length; i++) {
+        lost = result.rows;
+
+      res.render('index', {
+        most
+    })
+
+    } catch (err){
+        console.error(err);
+        res.send("Error" + err);
+    }
 })
 
-app.post('/post', function(req, res){
-    let text = req.body.postText
+app.post('/post', function (req, res) {
+    var text = req.body.userinput;
+    if (text == " "){
+        res.send('please enter something');
+    } else {
 
-        client.query('INSERT INTO posts (message) VALUES (\'' + text + '\')', (err, res) => {
-            if (err) {
-              console.log("err1")
-            } else {
-                console.log('\'' + text + '\' posted successfully')
+        client.query('INSERT INTO forum (message) VALUES ($1)', [text],  (err, res) => {
+            if (err) { console.log(err)}
+            else {
+                console.log("posted successfully");
             }
-          })
-    res.redirect('/')
+         })
+    }
+    res.redirect('/');
 })
 
 
 
-app.listen(process.env.PORT || 8000, function(){
-    console.log('port change')
-})
+
+  app.get('/cool', (req, res) => res.send(cool()))
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
